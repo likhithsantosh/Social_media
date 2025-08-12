0e3e6017bd3f8378d56572ed67e67a1d457157f4
likhithsantosh <156637680+likhithsantosh@users.noreply.github.com>
2025-08-12 14:03:35 +0530

    index.js

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-import { inngest, functions } from './inngest/Server.js'
 import { clerkMiddleware } from '@clerk/express';
+import { serve } from 'inngest';
 import connectDB from './config/db.js';
-import { serve } from "inngest";
+
+// Inngest
+import { inngest, functions } from './inngest/Server.js';
+
+// Routes
 import userRouter from './routes/userroutes.js';
 import postRouter from './routes/postroutes.js';
-import storyRouter from  './routes/storuroutes.js';
-import messageeRouter from './routes/messageroutes.js';
+import storyRouter from './routes/storuroutes.js';
+import messageRouter from './routes/messageroutes.js';
 
 const app = express();
+
+// Connect to Database
 await connectDB();
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(clerkMiddleware());
 
-app.use('/api/inneget', serve({client: inngest, functions}));
-app.use('/api/user', userRouter)
-app.use('/api/post',postRouter);
-app.use('/api/story', storyRouter)
-app.use('/api/message', messageeRouter);
+// Routes
+app.use('/api/inngest', serve({ client: inngest, functions }));
+app.use('/api/user', userRouter);
+app.use('/api/post', postRouter);
+app.use('/api/story', storyRouter);
+app.use('/api/message', messageRouter);
 
-app.listen(3000,()=>{
-    console.log('server is running on port 3000');
-});
+// Start Server (for local development only)
+app.listen(3000, () => {
+        console.log('Server running on port 3000');
+    });
 
-
+// For Vercel (export handler)
+export default app;
